a71cc9109d12f77693e8c352446961f63b55ca60
unknown <shuddhispace@gmail.com>
2026-01-08 12:20:30 +0530

    ne

--- models/Owner.js
+++ models/Owner.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
-const OwnerSchema = new mongoose.Schema({
+const ownerSchema = new mongoose.Schema({
   name: { type: String, required: true }
 }, { timestamps: true });
+
+module.exports = mongoose.model("Owner", ownerSchema);
